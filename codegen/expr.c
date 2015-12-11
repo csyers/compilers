b268a20b1e70b85d7f06385f3212b49b7a13e24a
Christopher Syers <csyers@nd.edu>
2015-12-11 00:53:43 -0500

    cannot modify global strings

--- codegen/expr.c
+++ codegen/expr.c
@@ -1081,7 +1081,11 @@ void expr_codegen( struct expr *e, FILE* f)
 		case EXPR_ARRAY:
 			// do nothing (supposed to be arrays)
 			break;
-		case EXPR_ASSIGN:		
+		case EXPR_ASSIGN:
+			if(e->left->symbol->kind == SYMBOL_GLOBAL && e->left->symbol->type->kind == TYPE_STRING){
+				printf("error: cannot modify global string (%s)\n\n",e->left->symbol->name);
+				exit(1);
+			}
 			expr_codegen(e->right,f);		// codegen the right
 			symbol_code(e->left->symbol,reg_name);	// get name of the left identifier
 			fprintf(f,"\tMOV %s, %s\n",register_name(e->right->reg),reg_name);	// move the value of the right into the symbol
